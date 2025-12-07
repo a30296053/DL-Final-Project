f11bfac86e9037c3b10380521609077eb37931d6
SortedApe <125339315+SortedApe@users.noreply.github.com>
2025-12-07 13:53:11 +0800

    change main
    add matplt
    fix train as input error

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from MODEL  import *
 from Config import  *
 from torch.utils.data import DataLoader
 import time
+import matplotlib.pyplot as plt
 import torch
+train_f1_list = []
+val_f1_list =[]
+
 def unison_shuffled(a, b):
     assert len(a) == len(b)
     p = np.random.permutation(len(a))
@@ -42,12 +46,14 @@ def train(t_dataset,v_dataset ,model:nn.Module):
             running_FP += ((pred == 1) & (y_true == 0)).sum().item()
             running_FN += ((pred == 0) & (y_true == 1)).sum().item()
 
-            
+        
         train_loss = running_loss / n
         precision = running_TP / (running_TP + running_FP + 1e-8)
         recall    = running_TP / (running_TP + running_FN + 1e-8)
         train_f1  = 2 * precision * recall / (precision + recall + 1e-8)
         val_loss, val_f1 = eval(v_dataset,model)
+        train_f1_list.append(train_f1)
+        val_f1_list.append(val_f1)
         print(f"Epoch {epoch:02d}/{EPOCH} | "
               f"Train Loss {train_loss:.4f} | Train F1 {train_f1:.4f} | "
               f"Val Loss {val_loss:.4f} | Val F1 {val_f1:.4f}")
@@ -101,9 +107,23 @@ def main():
     train_data = DRIAM(X[:ts], Y[:ts])
     valid_data  =  DRIAM(X[ts : ts+vs], Y[ts : ts+vs])
     test_data = DRIAM(X[ts+vs:],Y[ts+vs:])
-    train = input("Train y/n :")
-    if train == 'y':
+    INPUT = input("Train y/n :")
+    if INPUT == 'y':
         train(train_data, valid_data,model)
+        epochs = range(1, EPOCH + 1)
+
+        plt.figure(figsize=(8, 5))
+
+        plt.plot(epochs, train_f1_list, color='red', label='Train F1')
+        plt.plot(epochs, val_f1_list, color='blue', label='Validation F1')
+
+        plt.xlabel("Epoch")
+        plt.ylabel("F1 Score")
+        plt.title("F1 Score vs Epoch")
+        plt.legend()
+        plt.grid(True)
+
+        plt.show()
     else:
         try:
             model.load_state_dict(torch.load("./DRIAMS/best_DRIAM.pt"))
